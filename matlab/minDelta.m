4184c59dfb97eecfa768377641a35b9d09e35a26
Cica GUSTIANI <cicagustiani@newmail.lip6.fr>
2025-05-07 10:06:31 +0200

    works

--- matlab/minDelta.m
+++ matlab/minDelta.m
@@ -2,14 +2,14 @@ function [minDelta,C] = SDPnormtest
 tic
 N    = 8;
 din  = 2;
-dout = 3;
+dout = 2;
 
 rho_i = zeros(dout,dout,N);
 sigma_i = zeros(din,din,N);
 
 for i=1:N
-    rho_i(:,:,i)=RandomDensityMatrix(dout);
-    sigma_i(:,:,i)=RandomDensityMatrix(din);
+    rho_i(:,:,i)=RandomDensityMatrix(dout)
+    sigma_i(:,:,i)=RandomDensityMatrix(din)
 end
 
 yalmip('clear')
@@ -24,8 +24,20 @@ for i=1:N
 end
 minDelta=minDelta/N;
 
+ops = sdpsettings( ...
+        'solver'       , 'sedumi', ...
+        'sedumi.eps'   , 1e-8,       ... % tolerances
+        'sedumi.maxiter', 300 , ...
+        'sedumi.stepdif', 0   , ... % show every interior point step
+        'verbose'      , 2,   ...
+        'debug'        , 1,   ...   % keeps temporary files
+        'savesolverinput',1, ...
+        'savesolveroutput',1);
+
+
 % solution = solvesdp(F,minDelta,sdpsettings('verbose',1,'cachesolvers',1));
-solution = solvesdp(F,minDelta,sdpsettings('verbose',1,'cachesolvers',1,'solver','sedumi'));
+%solution = solvesdp(F,minDelta,sdpsettings('verbose',1,'cachesolvers',1,'solver','sedumi'));
+solution = solvesdp(F, minDelta, sdpsettings(ops))
 
 C = double(C);
 minDelta = double(minDelta);
